ee4b68e405eb900b51315f966da9e2c9e36e0b96
Ayden Mitchell <ayd3n1229@gmail.com>
2019-04-25 22:32:09 -0400

    window stuff

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
 import pygame
 
 def initWindow():
+    c = open(window.txt, "r")
+    if f.mode == "r":
+        contents = f.read()
+        print(contents)
     (width, height) = (300, 200)
     screen = pygame.display.set_mode((width, height))
     pygame.display.flip()
     input()
+
+def run():
+    initWindow()
