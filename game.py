3231cb0b025671ba116e1d3d75ffbc1d743cbca9
Ayden Mitchell <ayd3n1229@gmail.com>
2019-04-25 22:20:13 -0400

    added stuff

---
+++ game.py
@@ -0,0 +1,6 @@
+import pygame
+
+def initWindow():
+    (width, height) = (300, 200)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.flip()
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import pygame
+import game
+
+game.initWindow()
