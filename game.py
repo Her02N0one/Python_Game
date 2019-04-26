ecb7dc47b309101b6daca6f61339e4d323203ffa
Ayden Mitchell <ayd3n1229@gmail.com>
2019-04-25 22:44:41 -0400

    commit

--- game.py
+++ game.py
@@ -1,14 +1,25 @@
 import pygame
 
 def initWindow():
-    c = open(window.txt, "r")
-    if f.mode == "r":
-        contents = f.read()
-        print(contents)
-    (width, height) = (300, 200)
+    c = open("window.txt", "r")
+    if c.mode == "r":
+        config = c.readlines()
+        (width, height) = (config[1])
+
+
     screen = pygame.display.set_mode((width, height))
+    pygame.display.
     pygame.display.flip()
     input()
 
+def update():
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+
 def run():
     initWindow()
+    Running = True
+    while Running:
+        update()
